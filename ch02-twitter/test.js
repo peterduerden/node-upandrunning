614989e568ee1fa307c81b202f609d93b09ff354
Peter Duerden <peterduerden@gmail.com>
2012-10-03 19:15:16 +0100

    Added console messages to POST /send test.

--- ch02-twitter/test.js
+++ ch02-twitter/test.js
@@ -14,15 +14,17 @@ var req = http.request(opts, function(res){
   
   var data = '';
   res.on('data', function(d) {
-    console.log(d);
+    //console.log(d);
     data += d;
   });
   
   res.on('end', function() {
+    console.log('Testing POST /send');
     var json = "{\n" +
       "  \"status\": \"ok\",\n" +
       "  \"message\": \"Tweet received\"\n}";
     assert.strictEqual(data, json);
+    console.log('--> The response data was as expected');
   });
 });
 
