a04701c6da4605274547b507735e900c7c927a6a
Peter Duerden <peterduerden@gmail.com>
2012-10-03 09:38:46 +0100

    Renamed ch01-tcp-chat to ch02-tcp-chat.

---
+++ ch02-tcp-chat/chat.js
@@ -0,0 +1,46 @@
+var net = require('net');
+
+var chatServer = net.createServer(),
+  clientList = [];
+
+console.log('Starting TCP Chat Server...');
+
+chatServer.on('connection', function(client) {
+  client.name = client.remoteAddress + ':' + client.remotePort;
+  client.write('Hi! ' + client.name + '\n');
+  
+  clientList.push(client);
+  
+  client.on('data', function(data) {
+    broadcast(data, client);
+  });
+  
+  client.on('end', function() {
+    console.log('');
+    clientList.splice(clientList.indexOf(client), 1);
+  });
+});
+
+function broadcast(message, client) {
+  var cleanp = [];
+  
+  for(var i=0; i<clientList.length; i+=1) {
+    if(client != clientList[i]) {
+      if(clientList[i].writable) {
+        clientList[i].write(client.name + ' says ' + message);
+      } else {
+        cleanup.push(clientList[i]);
+        clientList[i].destroy();
+      }
+    }
+  }
+  
+  //Remove dead Nodes out of write loop to avoid trashing loop index
+  for(i=0; i<cleanup.length; i+=1) {
+    clientList.splice(clientList.indexOf(cleanup[i]), 1);
+  }
+}
+
+chatServer.listen(9000);
+
+console.log('TCP Chat Server listing on 127.0.0.1:9000...');
