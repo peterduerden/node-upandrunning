5e52c8561ffa6c05ac756173e32e86b722a7a9cd
Peter Duerden <peterduerden@gmail.com>
2012-10-03 07:46:14 +0100

    Example 2.7

--- ch01-tcp-chat/chat.js
+++ ch01-tcp-chat/chat.js
@@ -6,18 +6,23 @@ var chatServer = net.createServer(),
 console.log('Starting TCP Chat Server...');
 
 chatServer.on('connection', function(client) {
-  client.write('Hi!\n');
+  client.name = client.remoteAddress + ':' + client.remotePort;
+  client.write('Hi! ' + client.name + '\n');
   
   clientList.push(client);
   
   client.on('data', function(data) {
-    for(var i=0; i<clientList.length; i+=1) {
-      //Write this data to all clients
-      clientList[i].write(data);
-    }
-    
+    broadcast(data, client);
   });
 });
+
+function broadcast(message, client) {
+    for(var i=0; i<clientList.length; i+=1) {
+      if(client != clientList[i]) {
+        clientList[i].write(client.name + ' says ' + message);
+      }
+    }
+}
 
 chatServer.listen(9000);
 
