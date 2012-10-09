92446526c11dbef2bf20d7af5b4bf509c6582b1d
Peter Duerden <peterduerden@gmail.com>
2012-10-09 16:55:58 +0100

    Added examples 5-1, 5-2 and 5-3.

--- ch05-helper-apis/dns.js
+++ ch05-helper-apis/dns.js
@@ -1,9 +1,33 @@
-//Example 5-1. Calling dns.resolve( )
+//Example 5-1. Calling dns.resolve()
 var dns = require('dns');
 
-dns.resolve('yahoo.com', 'A', function(e,r) {
-  if (e) {
-    console.log(e);
-  }
-console.log(r);
+dns.resolve('yahoo.com', 'A', function(error, records) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(records);
+	}
+});
+
+//Example 5-2. Using resolve() versus resolveMx()
+dns.resolve('example.com', 'MX', function(error, records) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(records);
+	}
+});
+
+dns.resolveMx('example.com', function(error, records) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log(records);
+	}
+});
+
+//Example 5-3. Looking up a single A record with lookup()
+//DNS.lookup(domain, IP [4|6], callback)
+dns.lookup('google.com', 4, function(e, a) {
+  console.log(a);
 });
