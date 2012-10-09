ca396bb73975cf0dd02b92f365b1fab98edfc688
Peter Duerden <peterduerden@gmail.com>
2012-10-09 11:40:53 +0100

    Renamed to http-client-1.js.

---
+++ ch04-core-apis/http-client-1.js
@@ -0,0 +1,17 @@
+var http = require('http');
+
+var opts = {
+  host: 'www.google.co.uk',
+  port: 80,
+  path: '/',
+  method: 'GET'
+};
+
+var req = http.request(opts, function(res) {
+  console.log(res);
+  res.on('data', function(data) {
+    console.log(data);
+  });
+});
+
+req.end(); // Call to initiate request, else will wait expecting POST data.
