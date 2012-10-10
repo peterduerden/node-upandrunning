356f2c047f35ddcc62b66e779f854e9f89ae68de
Peter Duerden <peterduerden@gmail.com>
2012-10-10 22:37:16 +0100

    Added test for ok and notes about falsy and truthy.

--- ch05-helper-apis/assert-basic.js
+++ ch05-helper-apis/assert-basic.js
@@ -1,12 +1,30 @@
 //  Example 5-37. Basic assertions.
 
+/* Falsy
+ * 
+ * false
+ * 0
+ * null
+ * undefined
+ * empty string
+ * NaN
+ *
+ * Everything else is truthy!
+ */
+
 var assert = require('assert');
 
 assert.equal(1, true, '1 is equal to true'); // ==
 assert.notEqual(1, false, '1 is not equal to false'); // !=
 
-var isTrue = true;
-var isFalse = false;
-assert.strictEqual(isTrue, true, 'isTrue is identical to true'); // ===
-assert.notStrictEqual(isFalse, true, 'isFalse is not identical to true'); // !===
+var TRUE = true;
+var FALSE = false;
 
+assert.strictEqual(isTrue, true, 'TRUE is identical to true'); // ===
+assert.notStrictEqual(isFalse, true, 'FALSE is not identical to true'); // !===
+
+var someString = 'A non-empty string';
+var emptyString = '';
+
+assert.ok(someString, 'Strings that are not empty are truthy'); // ==
+assert.ok(0, 'Zero is not truthy')
