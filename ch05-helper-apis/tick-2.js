7adfa7f57321be89b921d7b8c3786805f4320213
Peter Duerden <peterduerden@gmail.com>
2012-10-10 14:17:13 +0100

    Added output comments.

--- ch05-helper-apis/tick-2.js
+++ ch05-helper-apis/tick-2.js
@@ -18,3 +18,10 @@ process.nextTick(function() {
 });
 
 console.log('End of 1st loop');
+
+// Results:
+// C:\node\node-upandrunning\ch05-helper-apis>node tick-2.js
+// End of 1st loop
+// nextTick 1
+// [ReferenceError: iAmAMistake is not defined]
+// nextTick 3
