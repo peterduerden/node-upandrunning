8b5ffb65ecf2d6b50d732aa416eedb2de848af4c
Peter Duerden <peterduerden@gmail.com>
2012-10-10 09:29:40 +0100

    Only execute getgid() and getuid() as only available on non-POSIX systems, i.e., not Windows.

--- ch05-helper-apis/current-process.js
+++ ch05-helper-apis/current-process.js
@@ -3,9 +3,22 @@ console.log(process.version);
 console.log(process.installPrefix); //undefined?
 console.log(process.platform);
 console.log(process.uptime());
-//console.log(process.getgid()); //No longer exists.
-//console.log(process.getuid()); //No longer exists.
+if (process.hasOwnProperty('getgid')) {
+  // http://nodejs.org/api/process.html#process_process_getgid
+  // Note: this function is only available on POSIX platforms (i.e. not Windows)
+  console.log(process.getgid());
+} else {
+  console.log('getgid() not available.')
+}
+if (process.hasOwnProperty('getuid')) {
+  // http://nodejs.org/api/process.html#process_process_getuid
+  // Note: this function is only available on POSIX platforms (i.e. not Windows)
+  console.log(process.getuid());
+} else {
+  console.log('getuid() not available.')
+}
 console.log(process.pid);
 console.log(process.title);
 console.log(process.execPath);
 console.log(process.cwd());
+console.log(process.memoryUsage());
