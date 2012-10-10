9b899971e9a5db83db9009d0a36f265b0c71e4ef
Peter Duerden <peterduerden@gmail.com>
2012-10-10 10:00:32 +0100

    Added argv.js.

---
+++ ch05-helper-apis/argv.js
@@ -0,0 +1,14 @@
+//Example 5-22. A simple script outputting argv
+console.log(process.argv);
+
+// Start at the command propmt with:
+// node argv.js -t 3 -c "abc def" -erf foo.js
+
+//[ 'node',
+//  'C:\\node\\node-upandrunning\\ch05-helper-apis\\argv.js',
+//  '-t',
+//  '3',
+//  '-c',
+//  'abc def',
+//  '-erf',
+//  'foo.js' ]
